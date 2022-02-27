83959e3b5bd88b8afd1d36895e2ac74f8d690f8b
dimitrilavin <dlavin@live.unc.edu>
2022-02-27 17:28:02 -0500

    server.js resolved

--- server.js
+++ server.js
@@ -41,27 +41,27 @@
 
 // That's it! You're all done!
 
-const http = require('http');
-const fs = require('fs');
-const args = require('minimist')([process.argv.slice(2)]);
-args['port'];
-const port = args.port || process.env.PORT || 3000;
+const http = require('http')
+const fs = require('fs')
+const args = require('minimist')([process.argv.slice(2)])
+args['port']
+const port = args.port || process.env.PORT || 3000
 
-fs.readFile('./www./index.html', 'utf8', (err, data) => {
+fs.readFile('./www/index.html', 'UTF-8', (err, data) => {
     if(err) {
-        console.error(err);
-        return;
-        process.exit(1);
+        console.error(err)
+        return
+        process.exit(1)
     }
 
     const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>./www./index.html/</h1>');
-    });
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        res.end(data)
+    })
     
     server.listen(port, () => {
         console.log(`Server listening on port ${port}`)
-      });
-});
+      })
+})
 
